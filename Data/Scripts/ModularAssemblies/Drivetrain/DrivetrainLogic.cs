0ac9d298b5aa3878114d6f3c0d9fcea9060182c6
Tracyn057 <39800501+Tracyn057@users.noreply.github.com>
2026-01-30 10:43:20 -0600

    Update DrivetrainLogic.cs

--- Data/Scripts/ModularAssemblies/Drivetrain/DrivetrainLogic.cs
+++ Data/Scripts/ModularAssemblies/Drivetrain/DrivetrainLogic.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace ModularAssemblies.Data.Scripts.ModularAssemblies.Drivetrain
 {
-    internal class DrivetrainLogic
+    internal class DrivetrainLogic : MyGameLogicComponent
     {
+
+        private int _assemblyId = -1;
+        private int _requestedPower;
+        private int _outputPower;
 
     }
 }
