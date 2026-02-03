833e451cb064153795b00e4a20642d42bcd67a83
Tracyn057 <39800501+Tracyn057@users.noreply.github.com>
2026-02-03 09:05:10 -0600

    Refactor GearboxLogic class and add initialization logic

--- Data/Scripts/ModularAssemblies/Drivetrain/GearboxLogic.cs
+++ Data/Scripts/ModularAssemblies/Drivetrain/GearboxLogic.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace NavalPowerSystems.Drivetrain
 {
-    internal class GearboxLogic
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_TerminalBlock), false,
+            "NPSDrivetrainMRG",
+            "NPSDrivetrainDRG"
+    )]
+    internal class GearboxLogic : MyGameLogicComponent
     {
+
+        internal static ModularDefinitionApi ModularApi => ModularDefinition.ModularApi;
+        private int _assemblyId = -1;
+        private IMyTerminalBlock _gearbox;
+        private bool _isComplete;
+        private bool _isReverse;
+        private bool _needsRefresh = true;
+        private bool _controlsInit = false;
+        private int _inputCount;
+        private int _outputCount;
+        
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            base.Init(objectBuilder);
+            _gearbox = Entity as IMyFunctionalBlock;
+
+            if (_gearbox == null) return;
+
+            _gearbox.AppendingCustomInfo += AppendCustomInfo;
+            NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+        }
+
+        public override void UpdateOnceBeforeFrame()
+        {
+
+            
+
+            NeedsUpdate |= MyEntityUpdateEnum.EACH_10TH_FRAME;
+        }
+
+        public override void UpdateBeforeSimulation10()
+        {
+            
+            
+            _gearbox.RefreshCustomInfo();
+        }
     }
 }
