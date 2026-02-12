17180b77645c7a5ee012e120b54541376fa725df
Tracyn057 <haze057@gmail.com>
2026-02-12 10:20:33 -0600

    Stuff

--- Data/Scripts/ModularAssemblies/Drivetrain/RudderLogic.cs
+++ Data/Scripts/ModularAssemblies/Drivetrain/RudderLogic.cs
@@ -24,5 +24,72 @@ namespace NavalPowerSystems.Drivetrain
         private float _currentAngle = 0f;
         private float _targetAngle = 0f;
         private float _currentThrottle = 0f;
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            base.Init(objectBuilder);
+            _rudder = (IMyTerminalBlock)Entity;
+            _myRudder = (MyCubeBlock)Entity;
+            NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+        }
+
+        public override void UpdateOnceBeforeFrame()
+        {
+            _rudder.AppendingCustomInfo += AppendCustomInfo;
+            NeedsUpdate |= MyEntityUpdateEnum.EACH_10TH_FRAME;
+            NeedsUpdate |= MyEntityUpdateEnum.EACH_FRAME;
+        }
+
+        public override void UpdateBeforeSimulation10()
+        {
+            _rudder.RefreshCustomInfo();
+        }
+
+        public override void UpdateBeforeSimulation()
+        {
+            GetAngle();
+            ApplyForce();
+        }
+
+        private void GetAngle()
+        {
+            
+        }
+
+        private void ApplyForce()
+        {
+            var grid = _myRudder.CubeGrid as MyCubeGrid;
+
+            if (grid.IsPreview || grid.Physics == null || !grid.Physics.Enabled || grid.Physics.IsStatic)
+                return;
+
+            Vector3D velocity = grid.Physics.LinearVelocity;
+            double speed = velocity.Length();
+
+            finalThrust *= 1000000f; // Convert MN to N for physics application
+
+            if (finalThrust >100)
+            {
+                Vector3D thrustVector = _myRudder.WorldMatrix.Right * (float)finalThrust;
+                var BlockPos = _myRudder.PositionComp.GetPosition();
+                grid.Physics.AddForce(
+                    MyPhysicsForceType.APPLY_WORLD_FORCE,
+                    thrustVector,
+                    BlockPos,
+                    null
+                );
+            }
+        }
+
+        private void AppendCustomInfo(IMyTerminalBlock block, StringBuilder sb)
+        {
+            sb.AppendLine($"Angle: {_currentAngle:F4} degrees");
+        }
+
+        public override void OnRemovedFromScene()
+        {
+            if (_rudder != null) _rudder.AppendingCustomInfo -= AppendCustomInfo;
+        }
+
     }
 }
