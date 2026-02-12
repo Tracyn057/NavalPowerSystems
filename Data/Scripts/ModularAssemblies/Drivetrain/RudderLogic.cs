661312b40efac7285296b386eec5044f3a773570
Tracyn057 <haze057@gmail.com>
2026-02-11 21:24:30 -0600

    Stuff

--- Data/Scripts/ModularAssemblies/Drivetrain/RudderLogic.cs
+++ Data/Scripts/ModularAssemblies/Drivetrain/RudderLogic.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using Sandbox.ModAPI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
 
 namespace NavalPowerSystems.Drivetrain
 {
-    internal class RudderLogic
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_TerminalBlock), false,
+            ""
+    )]
+    internal class RudderLogic : MyGameLogicComponent
     {
+        private IMyTerminalBlock _rudder;
+        private IMyCubeBlock _myRudder;
+        private GearboxLogic _gearboxLogic;
+
+        private bool _isAutoCenter = true;
+
+        private float _currentAngle = 0f;
+        private float _targetAngle = 0f;
+        private float _currentThrottle = 0f;
     }
 }
